cfeda6a3ff20843276b23eb5e327cbbe3fb6da0f
Elena Druzhinina <elenashebrova@gmail.com>
2022-04-29 14:14:41 +0700

    change-app

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,102 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <v-app class="main">
+    <MoviesHeader />
+    <MoviesLoader />
+    <MoviesNotification
+      :type-msg="typeMsg"
+      :msg="msg"
+      v-if="showNotification"
+    />
+    <PosterBg :poster="posterBg" />
+    <MoviesList :list="moviesList" @changePoster="onChangePoster" />
+    <MoviesPagination
+      :currentPage="currentPage"
+      :per-page="moviesPerPage"
+      :total="moviesLength"
+      @pageChanged="onPageChanged"
+    />
+  </v-app>
 </template>
 
+<script>
+import MoviesList from "@/components/MoviesList.vue";
+import PosterBg from "@/components/PosterBg.vue";
+import MoviesPagination from "@/components/MoviesPagination.vue";
+import MoviesLoader from "@/components/MoviesLoader.vue";
+import MoviesHeader from "@/components/MoviesHeader.vue";
+import MoviesNotification from "@/components/MoviesNotification.vue";
+import { mapActions, mapGetters, mapMutations } from "vuex";
+
+export default {
+  name: "App",
+  components: {
+    MoviesList,
+    PosterBg,
+    MoviesPagination,
+    MoviesLoader,
+    MoviesHeader,
+    MoviesNotification,
+  },
+  data: () => ({
+    posterBg: "",
+    msg: "",
+    typeMsg: "error",
+    showNotification: false,
+  }),
+  computed: {
+    ...mapGetters("movies", [
+      "moviesList",
+      "moviesPerPage",
+      "moviesLength",
+      "currentPage",
+      "msgError",
+    ]),
+  },
+  watch: {
+    "$route.query": {
+      handler: "onPageQueryChange",
+      immediate: true,
+      deep: true,
+    },
+    msgError: "showMsg",
+  },
+  methods: {
+    ...mapActions("movies", ["fetchMovies", "changeCurrentPage"]),
+    ...mapMutations("movies", ["MSG_ERROR"]),
+    onChangePoster(poster) {
+      this.posterBg = poster;
+    },
+    onPageChanged(page) {
+      this.$router.push({ query: { page } });
+    },
+    onPageQueryChange({ page = this.currentPage }) {
+      this.changeCurrentPage(Number(page));
+    },
+    showMsg() {
+      this.msg = this.msgError;
+      if (this.msg) {
+        this.showNotification = true;
+        setTimeout(() => {
+          this.showNotification = false;
+        }, 2500); 
+      }
+      setTimeout(() => {
+          this.MSG_ERROR("");
+        }, 2500);
+      
+    },
+  },
+};
+</script>
+
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+.main {
+  font-family: Arial, Helvetica, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
+  position: relative;
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
-import { createStore } from 'vuex'
+import { createStore } from "vuex";
+import movies from "@/store/modules/movies";
+import loader from "@/store/modules/loader";
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
+  state: {},
+  getters: {},
+  mutations: {},
+  actions: {},
   modules: {
-  }
-})
+    movies,
+    loader,
+  },
+});
